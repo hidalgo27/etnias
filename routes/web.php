71f6aa12663f58a84166c17b3d59240efc80aa4e
Hidalgo <hidalgochpnce@gmail.com>
2019-03-15 15:17:35 -0500

    Sesion con roles y middleware

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth', 'role:user');
 Route::get('/', [
     'uses' => 'HomepageController@index',
     'as' => 'home_path',
