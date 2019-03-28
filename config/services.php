6547d6f6145e9ac79c070198d6c5fd353caa86b8
Hidalgo <hidalgochpnce@gmail.com>
2019-03-28 12:01:02 -0500

    Se inseto id facebook

--- config/services.php
+++ config/services.php
@@ -42,7 +42,7 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'FACEBOOK_CALLBACK',
+        'redirect' => env('FACEBOOK_CALLBACK'),
     ],
 
 ];
