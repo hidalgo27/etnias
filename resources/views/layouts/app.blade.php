2b8ef47fd1d677bbd0b5fc5e268bad579d4ab9f3
Hidalgo <hidalgochpnce@gmail.com>
2019-03-28 13:44:31 -0500

    Updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    <img src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->name }}" height="64" width="64" class="rounded-circle"> {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
