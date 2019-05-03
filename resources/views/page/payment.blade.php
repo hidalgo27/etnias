65bf9554a23d1372a0014d8cbcee15d270d82f1c
Alien <hidalgochpnce@gmail.com>
2019-05-03 16:53:41 -0500

    Fusion realizad

--- resources/views/page/payment.blade.php
+++ resources/views/page/payment.blade.php
@@ -335,5 +335,9 @@
         window.location.hash="Again-No-back-button";//esta linea es necesaria para chrome
         window.onhashchange=function(){window.location.hash="no-back-button";}
 
+        window.location.hash="no-next-button";
+        window.location.hash="Again-No-next-button";//esta linea es necesaria para chrome
+        window.onhashchange=function(){window.location.hash="no-next-button";}
+
     </script>
 @endpush
