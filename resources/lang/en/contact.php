3c795f12a5b76a83d6a998b2c5c7038d2b55c757
Hidalgo <hidalgochpnce@gmail.com>
2019-05-24 17:41:33 -0500

    Idiomas

---
+++ resources/lang/en/contact.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'contactenos' => 'Contáctenos',
+    'h1_p' => 'Mi etnia es una plataforma que incentiva el turismo vivencial poniendo en contacto comunidades andinas y turistas que deseen conocer el Perú profundo.',
+    'somos_una' => 'Somos una plataforma para reservas de turismo vivenvial.',
+    'direccion' => 'Dirección',
+    'contacto' => 'CONTACTO',
+    'reservas' => 'Reservas',
+    'atención_cliente' => 'Atención al Cliente'
+];
