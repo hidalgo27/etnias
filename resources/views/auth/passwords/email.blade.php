8d42e01beb32dfde0e5aba7b391ac02e7ac6c301
Hidalgo Mac <hidalgoponce@icloud.com>
2019-07-06 21:01:21 -0500

    Üpdate

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -36,7 +36,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-g-red-primary text-white font-weight-bold">
-                                    @lang('send_password_reset_link')
+                                    @lang('client.send_password_reset_link')
                                 </button>
                             </div>
                         </div>
