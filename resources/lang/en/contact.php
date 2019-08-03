67932411a6c0fb2a764c85420fdd5758fcde8ed8
Hidalgo Mac <hidalgoponce@icloud.com>
2019-08-03 14:41:50 -0500

    Update

--- resources/lang/en/contact.php
+++ resources/lang/en/contact.php
@@ -6,5 +6,13 @@ return [
     'direccion' => 'Dirección',
     'contacto' => 'Contact',
     'reservas' => 'Reservations',
-    'atención_cliente' => 'Customer service'
+    'atención_cliente' => 'Customer service',
+
+    'subscr_h1' => 'Subscribe to our newsletter',
+    'subscr_p' => 'Want to be notified when our article is published? Enter your email address and name below to be the first to know.',
+    'name' => 'NAME',
+    'correo' => 'EMAIL',
+    'Signup' => 'Signup for newsletter now',
+    'dont_worry' => 'Don\'t Worry ! You will not be spammed',
+    'thank' => 'THANK YOU',
 ];
